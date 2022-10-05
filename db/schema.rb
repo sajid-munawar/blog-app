d86298eebea65e2f370299637648fc244ff979f5
sajid-munawar <sajidmunawar08@gmail.com>
2022-10-05 15:55:36 +0100

    Add foreign key and index

--- db/schema.rb
+++ db/schema.rb
@@ -42,5 +42,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_135608) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
