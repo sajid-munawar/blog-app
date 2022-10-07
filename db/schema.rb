f76dd3ca6817268902ee7b3e32a3900e06fd6b1d
sajid-munawar <sajidmunawar08@gmail.com>
2022-10-07 13:12:39 +0100

    Add model test for all models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_224141) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_135608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,14 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_224141) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.string "director"
-    t.integer "rate"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
